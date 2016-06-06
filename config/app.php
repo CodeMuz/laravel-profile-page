764e4a1505e511369c322ecf19a2388b83192f45
Murray Wynnes <mrwynnes@gmail.com>
2016-06-06 03:49:36 +0100

    added new relic monitoring apm

--- config/app.php
+++ config/app.php
@@ -147,7 +147,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        
         /*
          * Application Service Providers...
          */
@@ -156,6 +156,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Intouch\LaravelNewrelic\NewrelicServiceProvider::class,
     ],
 
     /*
